427bc82b2cefe613131c5b045d744cc9c1c8e26c
Wei Liu <wwwliu99@yahoo.com>
2018-06-06 12:53:23 -0400

    print it out

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,21 @@
+import random
+import sys
+import os
+
+print("Hello World")
+name="Wei"
+# one line comment
+
+'''
+multi line 
+comments
+'''
+print("from", name)
+
+quote="\"Always remember you are unique\""
+multilinequote='''just like
+everyone else'''
+
+print("%s %s %s" % ('I like the quote', quote, multilinequote))
+
+
